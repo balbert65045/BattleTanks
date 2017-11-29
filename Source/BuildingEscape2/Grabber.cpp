b40a093d62ec405a7106fc548f287f43b47788fb
THE_BA_MACHINE\Ben <bcalbert777@gmail.com>
2017-11-29 07:22:50 -0500

    Put in line Raycast and added default pawn

---
+++ Source/BuildingEscape2/Grabber.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Grabber.h"
+#include "Gameframework/Actor.h"
+#include "Engine/World.h"
+
+#include "DrawDebugHelpers.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("%s is reporting for duty"), *GetOwner()->GetName());
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// get the player viewpoint
+	APlayerController*  APlayer = GetWorld()->GetFirstPlayerController();
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	APlayer->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
+
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector()*Reach;
+
+	DrawDebugLine(GetWorld(), 
+		PlayerViewPointLocation, 
+		LineTraceEnd, 
+		FColor(255,0,0), 
+		false, 0, 0, 
+		12.333);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Location: %s  Rotation: %s"), 
+	//	*PlayerViewPointLocation.ToString(), 
+	//	*PlayerViewPointRotation.ToString());
+
+	// Ray-cast out to reach distance 
+
+	//see what we hit
+	
+}
+
